a2da090fa34741abee9214d8cf9924c7504d88e5
hadley31 <nick.hadley31@gmail.com>
2020-12-31 17:52:09 -0500

    file path

--- src/util/discord.js
+++ src/util/discord.js
@@ -1,6 +1,6 @@
-const logger = require("./logger")
-const fs = require("fs")
-const discord = require("discord.js")
+const logger = require('./logger')
+const fs = require('fs')
+const discord = require('discord.js')
 
 class DiscordUtil {
   /**
@@ -21,13 +21,8 @@ class DiscordUtil {
 
   static logMessage(message) {
     let user = DiscordUtil.getUserNameIDString(message.author)
-    let server = message.guild
-      ? `${message.guild.name} (${message.guild.id})`
-      : "none"
-    let channel =
-      server === "none"
-        ? message.channel.id
-        : DiscordUtil.getChannelNameIDString(message.channel)
+    let server = message.guild ? `${message.guild.name} (${message.guild.id})` : 'none'
+    let channel = server === 'none' ? message.channel.id : DiscordUtil.getChannelNameIDString(message.channel)
     logger.log(
       `Message (${message.id}):\n\tUser: ${user}\n\tServer: ${server}\n\tChannel: ${channel}\n\tContent: ${message.content}`
     )
@@ -40,42 +35,43 @@ class DiscordUtil {
    */
   static async play_sound(channel, file_path) {
     if (channel === undefined) {
-      logger.log("Channel undefined!")
+      logger.log('Channel undefined!')
+      return
+    }
+
+    if (file_path === undefined) {
+      logger.log('FILE PATH')
       return
     }
 
     try {
       const connection = await channel.join()
-      logger.log(
-        `\nJoined voice channel: ${DiscordUtil.getChannelNameIDString(
-          channel
-        )})`
-      )
+      logger.log(`\nJoined voice channel: ${DiscordUtil.getChannelNameIDString(channel)})`)
       const dispatcher = connection.play(fs.createReadStream(file_path), {
         highWaterMark: 1,
       })
 
-      dispatcher.on("start", () => {
-        logger.log("Started voice.")
+      dispatcher.on('start', () => {
+        logger.log('Started voice.')
         connection.player.streamingData.pausedTime = 0
       })
 
-      dispatcher.on("finish", () => {
-        logger.log("Finished voice.")
+      dispatcher.on('finish', () => {
+        logger.log('Finished voice.')
         dispatcher.destroy()
         connection.disconnect()
       })
 
-      dispatcher.on("error", (e) => {
-        logger.log("An error occured while playing a sound.")
+      dispatcher.on('error', (e) => {
+        logger.log('An error occured while playing a sound.')
         dispatcher.destroy()
         connection.disconnect()
       })
-      connection.on("disconnect", (err) => {
+      connection.on('disconnect', (err) => {
         dispatcher.destroy()
       })
     } catch (err) {
-      logger.log("An error occured while join voice.")
+      logger.log(`An error occured while joining ${DiscordUtil.getChannelNameIDString(channel)}`)
       logger.log(err)
     }
   }
