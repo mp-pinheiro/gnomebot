834c8a856c662fd85701625ac460f83c5f0974ee
hadley31 <nick.hadley31@gmail.com>
2021-06-06 16:06:44 -0400

    Updated help command

--- src/commands/help.js
+++ src/commands/help.js
@@ -5,16 +5,33 @@ const logger = new Logger("help")
 
 export default {
   name: 'help',
-  desc: 'Displays all commands and their descriptions.',
+  desc: 'Displays help for all or specific commands.',
   /**
    * @param {Message} message
    * @param {Array<String>} args
    */
   async execute(message, args) {
-    const m = message.client.commands
-      .mapValues((x) => `**${x.name}** - ${x.desc}`)
-      .array()
-      .join('\n')
-    message.reply(`here are my commands:\n${m}`)
+    if (args.length == 0) {
+      const m = message.client.commands
+        .mapValues((x) => `**${x.name}** - ${x.desc}`)
+        .array()
+        .join('\n')
+      return message.reply(`here are my commands:\n${m}`)
+    }
+
+    const c = args[0]
+
+    const command = message.client.commands.find(x => x.name.toLowerCase() == c.toLowerCase())
+
+    if (!command) {
+      return message.reply(`Couldn't resolve command: ${c}`)
+    }
+
+    if ('help' in command) {
+      return message.reply(`\n${command.help}`)
+    }
+    else{
+      return message.reply('that command does not have a detailed help description.')
+    }
   },
 }
