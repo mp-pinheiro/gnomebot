de362dda16d9afbd19035d1152784c54a853d136
GuiCoppini <guilhermecoppinip@gmail.com>
2022-09-30 00:47:05 -0300

    Remove add_replied_subcommand

--- src/modules/sub_command/quote/add_subcommand.js
+++ src/modules/sub_command/quote/add_subcommand.js
@@ -5,11 +5,10 @@ export default class AddSubcommand extends SubCommand {
   async process(params) {
     let message = params.message;
     let quoteMsg = this.parseParams(params.quoteMsg);
-
     try {
       if (quoteMsg !== null && quoteMsg !== '') {
         if (quoteMsg.length <= 300) {
-          await QuoteDAO.addQuote(message.author, quoteMsg);
+          await QuoteDAO.addQuote(params.author, quoteMsg);
           message.channel.send(`Poof! ${message.author}'s quote was added to the quote list.`);
         } else {
           message.channel.send(`Oof! That quote is too mighty and big. 300 characters only!`);
