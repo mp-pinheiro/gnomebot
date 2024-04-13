833343c4df67e4d69994495546766b6a6283d1bf
hadley31 <nick.hadley31@gmail.com>
2024-04-12 21:34:24 -0400

    update refresh deployment script

--- scripts/refresh_deployment.sh
+++ scripts/refresh_deployment.sh
@@ -3,8 +3,8 @@ if git merge-base --is-ancestor origin/main main; then
     echo No recent changes in main branch
 else
     echo Pulling latest changes from main branch
-    docker compose down gnomebot
     git pull origin main
     docker compose build
+    docker compose down gnomebot
     docker compose up -d gnomebot
 fi
